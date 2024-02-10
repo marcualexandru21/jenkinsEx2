2ca3e9b184e4185778e57993382409f24111d406
hamsamuxumed <hamsamuxumed3@gmail.com>
2024-02-10 23:18:05 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,9 @@
+def testApp() {
+    
+    echo 'testing the application..'
+    echo "Executing pipeline branch for $BRANCH_NAME"
+}
+
 def buildJar() {
  
     echo 'building the application..'
@@ -7,10 +13,18 @@ def buildJar() {
 def buildImage() {
     
     echo 'building the image..'
-    withCredentials([usernamePassword(credentialsID: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')])
+    withCredentials([
+        usernamePassword(credentialsID: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')
+    ])
         sh "docker build -t priv-docker-images:jma3.0"
         sh "echo $PASSWORD | docker login -u $USER --password-stdin"
         sh "docker push priv-docker-images:jma3.0"
+}
+
+
+def deployApp() {
+    
+    echo 'deploying the application..'
 }
 
 
