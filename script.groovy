803e834a44c2751197e10441dc0b8952cc3a7dd8
hamsamuxumed <hamsamuxumed3@gmail.com>
2024-02-10 21:57:29 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,11 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
+def buildApp() {
+    
+    echo 'building the application..'
+    sh "mvn install"
+    echo "Build version ${params.VERSION}"
 }
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
-    }
-}
+return this
 
 def deployApp() {
     echo 'deploying the application...'
