ee33af64e571b9a8cbe0eb536044e41192e6f8f6
Brinza <ionela.brinza@yahoo.com>
2024-02-12 01:11:08 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,17 @@
-def buildApp() {
-   echo 'building the application'
+def buildJar() {
+   echo 'building the jar'
+   sh 'mvn package'
 }
 
-def testApp() {
-   echo 'testing the application'
+def buildImage() {
+   echo 'building the docker image'
+   withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+      sh 'docker build -t nicolebrinza/twn-demo-app:jma-2.0 .'
+      sh 'echo $PASS | docker login -u $USER --password-stdin'
+      sh 'docker push nicolebrinza/twn-demo-app:jma-2.0'
 }
 
 def deployApp() {
    echo 'deploying the application'
-   echo "deploying version ${params.VERSION}"
 }
 return this
