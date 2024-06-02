9fb488106918b583982f2d7ed351d65904895e0e
Jerome Jay Geronimo <jeromejaygeronimo1@gmail.com>
2024-06-02 19:25:03 +0000

    Update 2 files
    - /Jenkinsfile
    - /script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
+def buildJar () {
     echo 'building the application...'
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo 'testing the application...'
+def buildImage() {
+     echo "building the docker image..."
+                    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                        sh 'docker build -t jeromejay09/demo-app:jma-2.0 .'
+                        sh 'echo $PASS | docker login -u $USER --password-stdin'
+                        sh 'docker push jeromejay09/demo-app:jma-2.0'
+                    }
 }
 
 def deployApp() {
-    echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
+    echo 'deploying docker image...'
 }
 
 return this
