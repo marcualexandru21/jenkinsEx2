7711e26e54434830ad446380d5ebedf4f9879fa5
Anas Zenagui <zenaguianas20@gmail.com>
2024-09-27 18:35:30 +0000

    Add new file

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildApp() {
+    echo "building the app..."
+}
+
+def buildApp() {
+    echo "testing the app..."
+}
+
+def buildApp() {
+    echo "deploying the app..."
+    echo "deploying version ${VERSION}"
+}
