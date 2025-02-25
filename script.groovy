6feb3c6b4c52ba92e91a0f8d044190b51f1479c6
Alexandru Marcu <139750314+marcualexandru21@users.noreply.github.com>
2025-02-25 11:31:51 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
-    echo 'building the application'
+def buildJar() {
+    echo "Building the jar"
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo 'testing the application...'
+def buildAndPushImage() {
+    echo "Building the docker image"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t mbradu/demo-app-twn:jma-2.0 .'
+        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+        sh 'docker push mbradu/demo-app-twn:jma-2.0'
+    }
 }
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
 }
 
 return this
